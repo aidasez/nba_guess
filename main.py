0910e17e3c22ee98080acdbc462610ae9e52aef5
aidasez <aidascoolify@gmail.com>
2025-11-20 14:19:33 +0000

    Update NBA HTML files

--- main.py
+++ main.py
@@ -365,19 +365,14 @@ def get_response_api():
         if not spreads.get("bookmakers"):
             continue
 
-        # moneyline bookmaker (you can pick the first, or also paddypower)
-        money_bm = next((bm for bm in money["bookmakers"] if bm["key"] == "paddypower"), None)
-        spread_bm = next((bm for bm in spreads["bookmakers"] if bm["key"] == "paddypower"), None)
+        money_bm = money["bookmakers"][0]
+        spread_bm = spreads["bookmakers"][0]
 
-        # skip if not found
-        if not money_bm or not spread_bm:
+        if not money_bm.get("markets"):
+            continue
+        if not spread_bm.get("markets"):
             continue
 
-        # make sure markets exist
-        if not money_bm.get("markets") or not spread_bm.get("markets"):
-            continue
-
-        # pick first market (h2h or spreads)
         money_market = money_bm["markets"][0]
         spread_market = spread_bm["markets"][0]
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-    money_bm = next((bm for bm in money["bookmakers"] if bm["key"] == "paddypower"), None)
-        spread_bm = next((bm for bm in spreads["bookmakers"] if bm["key"] == "paddypower"), None)
-
-        # skip if not found
-        if not money_bm or not spread_bm:
-            continue
-
-        # make sure markets exist
-        if not money_bm.get("markets") or not spread_bm.get("markets"):
-            continue
-
-        # pick first market (h2h or spreads)
-        money_market = money_bm["markets"][0]
-        spread_market = spread_bm["markets"][0]
+0
